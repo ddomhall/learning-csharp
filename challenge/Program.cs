986fd0b2ab75f2082b226b56577797292d8ed385
ddomhall <domjshall@gmail.com>
2024-05-17 18:37:45 +0100

    challenge

---
+++ challenge/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
+Console.Write("This is the first line.\n");
+Console.Write("This is the second line.\n");
