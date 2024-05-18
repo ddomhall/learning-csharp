983bf405b9afced46b2749b7128cffa8fd70d1e0
ddomhall <domjshall@gmail.com>
2024-05-18 10:25:53 +0100

    create files

--- mslearn-dotnet-files/Program.cs
+++ mslearn-dotnet-files/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
 
 IEnumerable<string> FindFiles(string folderName)
 {
