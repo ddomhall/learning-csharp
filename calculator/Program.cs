316b73a7fec24d3e12883b0077954f2090bd4eae
ddomhall <domjshall@gmail.com>
2024-05-24 18:18:12 +0100

    working sequentially

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,7 +1,43 @@
-﻿string x = "";
-while (!Int32.TryParse(x, out int b))
+﻿string? x = "";
+string? y = "";
+string? z = "";
+string[] operations = ["+", "-", "*", "/"];
+
+while (!Int32.TryParse(x, out int fallback))
 {
     Console.WriteLine("Enter number 1:");
     x = Console.ReadLine();
 }
-Console.WriteLine(x);
+
+while (!operations.Contains(y))
+{
+    Console.WriteLine("Select operation (+ - * /):");
+    y = Console.ReadLine();
+}
+
+while (!Int32.TryParse(z, out int fallback))
+{
+    Console.WriteLine("Enter number 2:");
+    z = Console.ReadLine();
+}
+
+Console.Write(x + y + z + "=");
+
+int a = Int32.Parse(x);
+int b = Int32.Parse(z);
+
+switch (y)
+{
+    case "+":
+        Console.WriteLine(a + b);
+        break;
+    case "-":
+        Console.WriteLine(a - b);
+        break;
+    case "*":
+        Console.WriteLine(a * b);
+        break;
+    case "/":
+        Console.WriteLine(a / b);
+        break;
+}
