cf6616802df22f9820e9d2d7a04c1d3968f1e589
ddomhall <domjshall@gmail.com>
2024-06-03 19:24:50 +0100

    icons + card layout

--- todo/client/src/components/Category.jsx
+++ todo/client/src/components/Category.jsx
@@ -3,12 +3,12 @@ import TodoCard from './TodoCard'
 export default function Category({ name, todos }) {
   return (
     <>
-      <section className='ring ring-white p-6 rounded-3xl gap-6 flex flex-col'>
+      <section className='ring ring-white p-6 rounded-3xl gap-6 flex flex-col w-96'>
         <h2 className='text-center border-b'>{name}</h2>
-        <div className='flex flex-col m-auto w-48 gap-6'>
+        <div className='flex flex-col m-auto gap-6'>
           {todos.map(todo => <TodoCard key={todo.id} todo={todo} />)}
         </div>
-        <button className='ring ring-white h-6 rounded-xl'>+</button>
+        <button className='ring ring-white h-16 text-3xl rounded-3xl'>+</button>
       </section>
     </>
   )
--- todo/client/src/components/TodoCard.jsx
+++ todo/client/src/components/TodoCard.jsx
@@ -1,9 +1,22 @@
+import { FaPencil } from "react-icons/fa6";
+import { FaXmark } from "react-icons/fa6";
+import { FaCheck } from "react-icons/fa6";
+
 export default function TodoCard({ todo }) {
   return (
-    <section className="flex flex-col w-48 p-6 gap-6 rounded-3xl ring ring-white">
-      <h3 className="text-center border-b">{todo.name}</h3>
-      <button className="ring ring-white rounded-xl">edit</button>
-      <button className="ring ring-white rounded-xl">delete</button>
-    </section>
+    <div className="flex p-3 w-80 rounded-3xl ring ring-white justify-between">
+      <h3 className="leading-10">{todo.name}</h3>
+      <div className="flex gap-3">
+        <button className="ring ring-white rounded-xl p-3">
+          <FaPencil size={"12px"} />
+        </button>
+        <button className="ring ring-white rounded-xl p-3">
+          <FaXmark />
+        </button>
+        <button className="ring ring-white rounded-xl p-3">
+          <FaCheck />
+        </button>
+      </div>
+    </div>
   )
 }
