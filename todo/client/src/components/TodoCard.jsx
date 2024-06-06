b67a59bc01d3418952ff77c44f02f966a36a7f18
ddomhall <domjshall@gmail.com>
2024-06-06 19:24:55 +0100

    todo edit mode

--- todo/client/src/components/TodoCard.jsx
+++ todo/client/src/components/TodoCard.jsx
@@ -1,22 +1,43 @@
+import { useState } from 'react'
 import { FaPencil } from "react-icons/fa6";
 import { FaXmark } from "react-icons/fa6";
 import { FaCheck } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa6";
 
 export default function TodoCard({ todo, deleteTodo }) {
+  const [edit, setEdit] = useState(false)
+
+  function cancelEdit(e) {
+    e.preventDefault()
+    setEdit(false)
+  }
+
+  function updateTodo(e) {
+    e.preventDefault()
+    console.log(e)
+    setEdit(false)
+  }
+
   return (
-    <div className="flex p-3 w-80 rounded-3xl ring ring-white justify-between">
-      <h3 className="leading-10">{todo.name}</h3>
-      <div className="flex gap-3">
-        <button className="ring ring-white rounded-xl p-3 w-10">
-          <FaPencil size={"12px"} />
-        </button>
-        <button onClick={() => deleteTodo(todo.id)} className="ring ring-white rounded-xl p-3">
-          <FaXmark />
-        </button>
-        <button className="ring ring-white rounded-xl p-3">
-          <FaCheck />
-        </button>
-      </div>
-    </div>
+    <>
+      {edit ?
+        <form onSubmit={updateTodo} className="flex p-3 w-80 rounded-3xl ring ring-white justify-between">
+          <input defaultValue={todo.name} className='ring ring-white rounded-xl w-48 h-10' />
+          <div className="flex gap-3">
+            <button onClick={cancelEdit} className="ring ring-white rounded-xl p-3"> <FaXmark /> </button>
+            <button type='submit' className="ring ring-white rounded-xl p-3"> <FaCheck /> </button>
+          </div>
+        </form>
+        :
+        <div className="flex p-3 w-80 rounded-3xl ring ring-white justify-between">
+          <h3 className="leading-10">{todo.name}</h3>
+          <div className="flex gap-3">
+            <button onClick={() => deleteTodo(todo.id)} className="ring ring-white rounded-xl p-3"> <FaTrash /> </button>
+            <button onClick={() => setEdit(true)} className="ring ring-white rounded-xl p-3 w-10"> <FaPencil size={"12px"} /> </button>
+            <button className="ring ring-white rounded-xl p-3"> <FaCheck /> </button>
+          </div>
+        </div>
+      }
+    </>
   )
 }
