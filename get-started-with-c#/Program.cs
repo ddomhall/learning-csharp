91042d63dd40ac5342c89b56dcb1aa9ccf4bb9ca
ddomhall <domjshall@gmail.com>
2024-06-19 07:38:51 +0100

    methods

--- get-started-with-c#/Program.cs
+++ get-started-with-c#/Program.cs
@@ -3,3 +3,5 @@ Part1.stringFormatting();
 Part1.numberOperations();
 Part1.studentGrades();
 Part1.calculateGpa();
+
+Part2.methods();
---
+++ get-started-with-c#/part2/methods.cs
@@ -0,0 +1,37 @@
+partial class Part2
+{
+    public static void methods()
+    {
+
+        // stateful vs stateless methods
+
+        // Random dice = new Random();
+        // int roll = dice.Next(1, 7);
+        // Console.WriteLine(roll);
+
+        // overloaded methods
+
+        // int number = 7;
+        // string text = "seven";
+
+        // Console.WriteLine(number);
+        // Console.WriteLine();
+        // Console.WriteLine(text);
+
+        // int roll1 = dice.Next();
+        // int roll2 = dice.Next(101);
+        // int roll3 = dice.Next(50, 101);
+
+        // Console.WriteLine($"First roll: {roll1}");
+        // Console.WriteLine($"Second roll: {roll2}");
+        // Console.WriteLine($"Third roll: {roll3}");
+
+        // challenge
+
+        // int firstValue = 500;
+        // int secondValue = 600;
+        // int largerValue = Math.Max(firstValue, secondValue);
+        // Console.WriteLine(largerValue);
+
+    }
+}
