eeb5519fcfd4f720957549cd4b2509a6e981bd01
ddomhall <domjshall@gmail.com>
2024-06-26 08:43:15 +0100

    for statement

---
+++ get-started-with-c#/part3/for-statement.cs
@@ -0,0 +1,25 @@
+partial class Part3
+{
+	public static void forStatement()
+	{
+
+		/*
+		string[] names = { "Alex", "Eddie", "David", "Michael" };
+		for (int i = 0; i < names.Length; i++)
+			if (names[i] == "David") names[i] = "Sammy";
+
+		foreach (var name in names) Console.WriteLine(name);
+
+		// challenge
+
+		for (int i = 1; i <= 100; i++)
+		{
+			if (i % 3 == 0 && i % 5 == 0) Console.WriteLine(i + "\tFizzBuzz");
+			else if (i % 3 == 0) Console.WriteLine(i + "\tFizz");
+			else if (i % 5 == 0) Console.WriteLine(i + "\tBuzz");
+			else Console.WriteLine(i);
+		}
+		*/
+
+	}
+}
