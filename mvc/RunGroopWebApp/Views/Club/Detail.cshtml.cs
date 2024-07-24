dfa12fb97bcddbb17b14a75020a4d70fc4a49ad1
Dom Hall <domjshall@gmail.com>
2024-07-24 21:57:31 +0100

    detail

---
+++ mvc/RunGroopWebApp/Views/Club/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RunGroopWebApp.Views.Club
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
