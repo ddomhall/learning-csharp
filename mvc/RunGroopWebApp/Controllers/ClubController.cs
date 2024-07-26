c53e6f0a966a5c106367bf6b8da1075c6cd84caf
Dom Hall <domjshall@gmail.com>
2024-07-26 22:49:51 +0100

    create

--- mvc/RunGroopWebApp/Controllers/ClubController.cs
+++ mvc/RunGroopWebApp/Controllers/ClubController.cs
@@ -26,5 +26,20 @@ namespace RunGroopWebApp.Controllers
             return View(club);
         }
 
+        public IActionResult Create(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
     }
 }
