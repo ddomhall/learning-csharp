aa5f8ba7b1186ce3d0cc903e0aea0be38c342468
Dom Hall <domjshall@gmail.com>
2024-08-04 22:47:28 +0100

    register

--- mvc/RunGroopWebApp/Controllers/AccountController.cs
+++ mvc/RunGroopWebApp/Controllers/AccountController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Models;
 using RunGroopWebApp.ViewModels;
-using RunGroopWebApp.Views.Account;
-using System.Runtime.InteropServices;
 
 namespace RunGroopWebApp.Controllers
 {
@@ -19,6 +17,7 @@ namespace RunGroopWebApp.Controllers
             _signInManager = signInManager;
             _userManager = userManager;
         }
+
         public IActionResult Login()
         {
             var response = new LoginViewModel();
@@ -34,20 +33,64 @@ namespace RunGroopWebApp.Controllers
 
             if (user != null)
             {
+                //User is found, check password
                 var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
                 if (passwordCheck)
                 {
+                    //Password correct, sign in
                     var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
                     if (result.Succeeded)
                     {
                         return RedirectToAction("Index", "Race");
                     }
                 }
-                TempData["Error"] = "Wrong credentials. Please, try again";
+                //Password is incorrect
+                TempData["Error"] = "Wrong credentials. Please try again";
                 return View(loginViewModel);
             }
-            TempData["Error"] = "Wrong credentials. Please, try again";
+            //User not found
+            TempData["Error"] = "Wrong credentials. Please try again";
             return View(loginViewModel);
         }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerViewModel);
+            }
+
+            var newUser = new AppUser()
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return RedirectToAction("Index", "Race");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Race");
+        }
+
+
     }
 }
