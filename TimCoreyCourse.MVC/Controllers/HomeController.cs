bd641f14030936d3649bdf920eff91e1c90e1432
ddomhall <domjshall@gmail.com>
2024-11-01 14:37:07 +0000

    mvc

---
+++ TimCoreyCourse.MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TimCoreyCourse.MVC.Models;
+
+namespace TimCoreyCourse.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Person()
+        {
+
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public IActionResult Person(Person person)
+        {
+            return View(person);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TimCoreyCourse.MVC/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimCoreyCourse.MVC.Models
+{
+    public class Person
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int? Age { get; set; }
+    }
+}
